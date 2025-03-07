1ddbe3332a168a271191a5f66cd9ba42a633563d
Milo Davis <Milo.Davis@nrel.gov>
2025-03-07 11:05:21 -0700

    Created parabola.stinput test and a test using powertower optimization on the powertower sample

---
+++ google-tests/csv_tools/split_csv.h
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include <iostream>
+#include <iomanip>
+#include <fstream>
+#include <sstream>
+#include <cmath>
+
+#include <string>
+#include <cstring>
+#include <vector>
+#include <exception>
+#include <cstdlib>
+
+using namespace std;
+
+extern vector<vector<string>> split_csv(string path);
