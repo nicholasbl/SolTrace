a7e490b4a7bac6a0f65ee731e4c3d2adbf6809a5
Milo Davis <Milo.Davis@nrel.gov>
2025-03-14 17:30:05 -0600

    Removed output messages from st_sim_run_test

--- google-tests/soltrace_example_tests/st_sim_run_test.cpp
+++ google-tests/soltrace_example_tests/st_sim_run_test.cpp
@@ -27,20 +27,9 @@ TEST(st_sim_run_test, BasicAssertions)
 {
 	cout << "Project Directory: " << PROJECT_DIR << endl;
 	string project_dir = PROJECT_DIR;
-	//if (length == 0) {
-	//	std::cerr << "Error getting current directory!" << std::endl;
-	//}
-	//else {
-	//	std::cout << "Current working directory: " << buffer << std::endl;
-	//}
-	cout << "Current working directory: " << project_dir << endl;
 
-	string additional_path = "/Aperture Examples.stinput";
-	project_dir += additional_path;
-
-	cout << "File path: " << project_dir << endl;
-
-
+	string sample_path = PROJECT_DIR + string("/Aperture Examples.stinput");
+	
 	const char* file = project_dir.data();
 	int nrays = 1;
 	int maxrays = 50;
@@ -89,20 +78,10 @@ TEST(st_sim_run_test, BasicAssertions)
 	int* stage_map = new int[Length];
 
 	// Retrieving data from Soltrace simulation
-
-	// Retrieving ray intersection locations
 	::st_locations(cxt, x_location, y_location, z_location);
-
-	// Retrieving cosines of ray intersections
 	::st_cosines(cxt, x_cos, y_cos, z_cos);
-
-	// Retrieving ray numbers for each ray
 	::st_raynumbers(cxt, ray_data);
-
-	// Retrieving element of simulation that each ray hit
 	::st_elementmap(cxt, element_map);
-
-	// Retrieving stage each ray is in during simulation
 	::st_stagemap(cxt, stage_map);
 	
 
@@ -111,21 +90,17 @@ TEST(st_sim_run_test, BasicAssertions)
 		EXPECT_EQ(ray_data[i], 1);
 		EXPECT_EQ(stage_map[i], 1);
 	}
-	cerr << "Ray numbers and stage numbers match." << endl;
 
 	EXPECT_EQ(element_map[0], 1);
 	
-	cerr << "Elements each ray hits match." << endl;
 
 	EXPECT_NEAR(x_location[0], -3.06214, 0.0001);
 	EXPECT_NEAR(y_location[0], 5.92862, 0.0001);
 	EXPECT_NEAR(z_location[0], 12.7732, 0.0001);
-	cerr << "All intersection positions match." << endl;
 
 	EXPECT_NEAR(x_cos[0], 1.22465e-16, 0.0000000000000001);
 	EXPECT_NEAR(y_cos[0], 0, 0.0001);
 	EXPECT_NEAR(z_cos[0], -1, 0.0001);
-	cerr << "All intersection cosines match." << endl;
 
 	EXPECT_EQ(Length, 1);
 }
