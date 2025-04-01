b89b848b4d321822fd1f5f8e62321f05f5d5c1a0
Milo Davis <Milo.Davis@nrel.gov>
2025-04-01 12:09:55 -0600

    Modified comments on parabola test and refactored to conform to other test structures.

--- google-tests/soltrace_example_tests/parabola_test.cpp
+++ google-tests/soltrace_example_tests/parabola_test.cpp
@@ -6,32 +6,20 @@
 
 using namespace std;
 
-// This test does not seem to work, location values are different from ground values 
-// taken from Soltrace executable. The parabola.mon file appears to be being loaded correctly.
 
-// Should test with fewer rays and check values more closely, see if cos values differ
 TEST(st_parabola_sim_run_test, BasicAssertions)
 {
-	cout << "Project Directory: " << PROJECT_DIR << endl;
-	string project_dir = PROJECT_DIR;
-	
-	cout << "Current working directory: " << project_dir << endl;
+	string sample_path = string(PROJECT_DIR) + string("/parabola.stinput");
 
 	//parabola.mon is loaded through input.cpp in coretrace. To work in Visual Studio debug,
 	//must be copied to debug directory in google-tests
-	string file_path = "/parabola.stinput";
-	string csv_path = "/parabola_example_raydata.csv";
-
-	string input_file_path = project_dir + file_path;
-	string ground_csv_path = project_dir + csv_path;
-
-	cout << "File path: " << project_dir << endl;
+	string ground_csv_path = PROJECT_DIR + string("/parabola_example_raydata.csv");
 
 	std::ifstream csv_file(ground_csv_path);
-	vector<vector<string>> result = split_csv(ground_csv_path);
+	vector<vector<string>> ground_raydata = split_csv(ground_csv_path);
 
 
-	const char* sample_file_path = input_file_path.data();
+	const char* sample_file_path = sample_path.data();
 	int nrays = 10000;
 	int maxrays = 100000;
 	int seed = 1;
@@ -79,35 +67,25 @@ TEST(st_parabola_sim_run_test, BasicAssertions)
 	int* stage_map = new int[Length];
 
 	// Retrieving data from Soltrace simulation
-
-	// Retrieving ray intersection locations
 	::st_locations(cxt, x_location, y_location, z_location);
-
-	// Retrieving cosines of ray intersections
 	::st_cosines(cxt, x_cos, y_cos, z_cos);
-
-	// Retrieving ray numbers for each ray
 	::st_raynumbers(cxt, ray_numbers);
-
-	// Retrieving element of simulation that each ray hit
 	::st_elementmap(cxt, element_map);
-
-	// Retrieving stage each ray is in during simulation
 	::st_stagemap(cxt, stage_map);
 
 	for (size_t i = 0; i < Length; i++)
 	{
-		ASSERT_NEAR(x_location[i], stod(result[0][i + 1]), 0.01);
-		ASSERT_NEAR(y_location[i], stod(result[1][i + 1]), 0.01);
-		ASSERT_NEAR(z_location[i], stod(result[2][i + 1]), 0.01);
+		ASSERT_NEAR(x_location[i], stod(ground_raydata[0][i + 1]), 0.01);
+		ASSERT_NEAR(y_location[i], stod(ground_raydata[1][i + 1]), 0.01);
+		ASSERT_NEAR(z_location[i], stod(ground_raydata[2][i + 1]), 0.01);
 
-		EXPECT_NEAR(x_cos[i], stod(result[3][i + 1]), 0.01);
-		EXPECT_NEAR(y_cos[i], stod(result[4][i + 1]), 0.01);
-		EXPECT_NEAR(z_cos[i], stod(result[5][i + 1]), 0.01);
+		EXPECT_NEAR(x_cos[i], stod(ground_raydata[3][i + 1]), 0.01);
+		EXPECT_NEAR(y_cos[i], stod(ground_raydata[4][i + 1]), 0.01);
+		EXPECT_NEAR(z_cos[i], stod(ground_raydata[5][i + 1]), 0.01);
 
-		EXPECT_NEAR(element_map[i], stod(result[6][i + 1]), 0.01);
-		EXPECT_EQ(stage_map[i], stod(result[7][i + 1]));
-		EXPECT_EQ(ray_numbers[i], stod(result[8][i + 1]));
+		EXPECT_NEAR(element_map[i], stod(ground_raydata[6][i + 1]), 0.01);
+		EXPECT_EQ(stage_map[i], stod(ground_raydata[7][i + 1]));
+		EXPECT_EQ(ray_numbers[i], stod(ground_raydata[8][i + 1]));
 
 	}
 
