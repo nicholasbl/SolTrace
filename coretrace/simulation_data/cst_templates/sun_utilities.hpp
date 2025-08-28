5ba5fc6f96187f7ff6d1f3e98840bea71c675109
Jonathan Maack <jonathan.maack@nrel.gov>
2025-08-28 13:34:30 -0600

    Fix windows build

--- coretrace/simulation_data/cst_templates/sun_utilities.hpp
+++ coretrace/simulation_data/cst_templates/sun_utilities.hpp
@@ -4,6 +4,10 @@
 
 #include "vector3d.hpp"
 
+#ifndef M_PI
+#define M_PI 3.141592653589793238462643
+#endif
+
 void sun_position_vector_degrees(Vector3d &sun_pos,
                                  double azimuth,
                                  double elevation);
