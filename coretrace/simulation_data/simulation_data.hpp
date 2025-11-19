0ec74688bfc7a80496d020bac1246c679bc9fd71
Taylor Brown <60201147+taylorbrown75@users.noreply.github.com>
2025-11-19 16:34:34 -0700

    Fix merge bug.

--- coretrace/simulation_data/simulation_data.hpp
+++ coretrace/simulation_data/simulation_data.hpp
@@ -274,8 +274,8 @@ public:
     int update_simulation_positions(const Date &);
     int update_simulation_positions(const DateTime &);
 
-    int import_from_file(const char *file_name);
-    int import_from_file(const std::string file_name);
+    bool import_from_file(const char *file_name);
+    bool import_from_file(const std::string file_name);
     void import_json_file(const std::string file_name);
     void export_json_file(const std::string file_name);
 
