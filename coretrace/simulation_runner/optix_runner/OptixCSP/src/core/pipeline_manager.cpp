fa3b64c798effed0da659aa9fe7ffa59ed9a26bf
Jonathan Maack <jonathan.maack@nrel.gov>
2026-03-27 16:19:10 -0600

    Add intersection function to pipeline manager map

--- coretrace/simulation_runner/optix_runner/OptixCSP/src/core/pipeline_manager.cpp
+++ coretrace/simulation_runner/optix_runner/OptixCSP/src/core/pipeline_manager.cpp
@@ -42,7 +42,8 @@ const std::map<OpticalEntityType, std::string> IntersectionKernelMap = {
     {OpticalEntityType::RECTANGLE_FLAT, "__intersection__rectangle_flat"},
     {OpticalEntityType::TRIANGLE_FLAT, "__intersection__triangle_flat"},
     {OpticalEntityType::CYLINDRICAL, "__intersection__cylinder_y"},
-    {OpticalEntityType::QUADRILATERAL_FLAT, "__intersection__quadrilateral_flat"}};
+    {OpticalEntityType::QUADRILATERAL_FLAT, "__intersection__quadrilateral_flat"},
+    {OpticalEntityType::CIRCLE_FLAT, "__intersection__circle_flat"}};
 
 pipelineManager::pipelineManager(SoltraceState &state) : m_state(state) {}
 
