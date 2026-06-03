0c3fe00e5d734ad2b7f4e5e78c3e909bfa796586
Nicholas Brunhart-Lupo <nicholasbl@icloud.com>
2026-06-03 14:46:19 -0600

    Add more flux stats

--- gui/src/analysis/baked_flux_map.h
+++ gui/src/analysis/baked_flux_map.h
@@ -3,8 +3,39 @@
 #include "utilities/grid2d.h"
 
 #include <QImage>
+#include <QtGlobal>
+#include <QVector3D>
 
 namespace analysis {
+
+/// Summary statistics computed with the baked flux map.
+struct BakedFluxMapStats {
+    /// Total rays in the simulation result used for this map.
+    quint64 source_ray_count = 0;
+
+    /// Number of plotted ray/surface interactions that contributed to this map.
+    quint64 plotted_ray_count = 0;
+
+    /// Current normalized energy per plotted ray interaction.
+    double power_per_ray = 1.0;
+
+    /// Integrated normalized power represented by plotted interactions.
+    double plotted_power = 0.0;
+
+    /// Flux statistics over the rasterized map values.
+    double peak_flux    = 0.0;
+    double min_flux     = 0.0;
+    double average_flux = 0.0;
+    double sigma_flux   = 0.0;
+    double uniformity   = 0.0;
+
+    /// Monte Carlo uncertainty estimates in percent where counts are known.
+    double peak_flux_uncertainty    = 0.0;
+    double average_flux_uncertainty = 0.0;
+
+    /// Average world-space location of plotted interactions.
+    QVector3D centroid;
+};
 
 /// A computed flux map
 struct BakedFluxMap {
@@ -16,6 +47,9 @@ struct BakedFluxMap {
 
     /// A generated image showing UV points of intersecting rays
     QImage point_map;
+
+    /// Summary statistics for the generated map.
+    BakedFluxMapStats stats;
 };
 
 using BakedFluxMapPtr = std::shared_ptr<BakedFluxMap const>;
